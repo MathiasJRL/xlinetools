db5a996026e0d0553600f3b7115f26ef09781871
MathiasJRL <59805389+MathiasJRL@users.noreply.github.com>
2026-01-23 21:04:29 +0100

    Update xlinetools.cpp

--- xlinetools.cpp
+++ xlinetools.cpp
@@ -1,23 +1,10 @@
-/* 
+/*
  * InspIRCd -- Internet Relay Chat Daemon
  *
- *   Copyright (C) 2018-2020 Matt Schatz <genius3000@g3k.solutions>
- *
- * This file is a module for InspIRCd.  InspIRCd is free software: you can
- * redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, version 2.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * X-line management tools: XCOUNT, XSEARCH, XREMOVE, XCOPY
  */
 
-/// $ModAuthor: genius3000
-/// $ModAuthorMail: genius3000@g3k.solutions
+/// $ModAuthor: MathiasJRL
 /// $ModDepends: core 4
 /// $ModDesc: X-line management with XCOPY, XCOUNT, XREMOVE, and XSEARCH
 
